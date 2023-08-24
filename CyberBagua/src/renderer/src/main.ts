20d4c41a997fd86cf48fb47e3a659419d83c3558
hyz-a <65569160@qq.com>
2023-08-24 23:19:32 +0800

    装包

--- CyberBagua/src/renderer/src/main.ts
+++ CyberBagua/src/renderer/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入ant-design ui框架
+import Antd from 'ant-design-vue';
+// 引入样式
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
